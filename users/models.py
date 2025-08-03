6b5f2229e057cffecb4266881ef3cdda21e34fee
fellow.25summer.ashika.shrestha <fellow.25summer.ashika.shrestha@cloco.net>
2025-08-03 12:45:48 +0545

    feat: Admin-panel task completed

--- users/models.py
+++ users/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
 
     ROLE_CHOICES= (
-        ( 'admin', 'Admin'),
         ('teacher', 'Teacher'),
         ('Student','student',) 
 
